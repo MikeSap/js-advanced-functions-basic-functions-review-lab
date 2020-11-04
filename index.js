553413f198475fd2444e9b357f4cfbe7c730c92d
mike <mikesapienzaaudio@gmail.com>
2020-11-04 11:36:13 -0600

    finished

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Your code here
+function saturdayFun(activity='roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+}
+
+let mondayWork = function(activity='go to the office'){
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(flair='*'){
+    return function(thing='special'){
+        return `You are ${flair}${thing}${flair}!`
+    }
+}
+
+let Calculator = {}
+
+Calculator.add = function(){
+    return 1+3
+}
+
+Calculator.subtract = () => 1-3
+
+Calculator.multiply = () => 1*3
+
+Calculator.divide = () => 10/5
+
+function actionApplyer(int, array){
+    if (array[int] === undefined){
+    return int
+    } else if (array[int] === 13){
+        return 4
+    }
+}   
